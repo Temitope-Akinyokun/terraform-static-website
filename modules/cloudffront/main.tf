b0fbc935ee027be751d90636ec013f75e5c2f784
Tope Akinyokun <tohpeakin@gmail.com>
2024-06-28 23:53:33 +0100

    Update cloudfront module

--- modules/cloudffront/main.tf
+++ modules/cloudffront/main.tf
@@ -74,7 +74,7 @@ resource "aws_cloudfront_distribution" "cloudfront" {
   viewer_certificate {
     acm_certificate_arn = var.certificate_arn
     ssl_support_method = "sni-only"
-    minimum_protocol_version = "TLSv.2_3821"
+    minimum_protocol_version = "TLSv.2_2021"
   }
 
 aliases = [ var.domain_name ]
